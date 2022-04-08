a0568476e6497d543f4d2daa48c6919f49938c44
Sims <eznobyen@gmail.com>
2022-04-08 17:38:09 +0800

    lt transaction frame start point need to fix

--- HighLevelAnalyzer.py
+++ HighLevelAnalyzer.py
@@ -14,8 +14,11 @@ class Hla(HighLevelAnalyzer):
     my_frame_string = str()
     start_time = None
     end_time = None
+    lt_start_time = None
+    lt_end_time = None
     frame_flip = int()
     temp = int()
+    status = int()
     data_count_between_data_link_escape = int()
     raw_data_array = []
     # An optional list of types this analyzer produces, providing a way to customize the way frames are displayed in Logic 2.
@@ -56,28 +59,43 @@ class Hla(HighLevelAnalyzer):
                 self.data_count_between_data_link_escape = 0
                 self.raw_data_array.clear()
                 self.start_time = frame.start_time
+                self.lt_start_time = frame.start_time
                 self.my_frame_string = None
                 self.my_frame_string = str()
+                return AnalyzerFrame("end", frame.start_time,frame.end_time, {
+                        'input_type': self.temp
+                    })
             else:
                 stx_symbol = int.from_bytes(
                     self.raw_data_array[0], byteorder='big')
                 self.data_count_between_data_link_escape = 0
-                self.end_time = frame.start_time
 
                 if (stx_symbol & 0xc0) == 0x00:
+                    self.end_time = frame.start_time
+                    self.status = 0
                     self.AT_decode()
                     return AnalyzerFrame("AT", self.start_time, self.end_time, {
                         'input_type': self.my_frame_string
                     })
                 elif (stx_symbol & 0xc0) == 0x40:
+                    self.end_time = frame.start_time
+                    self.status = 1
                     self.broadcast_or_addressed_RT_decode()
                     return AnalyzerFrame("RT", self.start_time, self.end_time, {
                         'input_type': self.my_frame_string
                     })
                 elif (stx_symbol & 0xc0) == 0x80:
+                    print(self.raw_data_array)
+                    self.status = 2
                     self.LT_decode()
-                    return AnalyzerFrame("LT", self.start_time, self.end_time, {
-                        'input_type': self.my_frame_string
+                    self.raw_data_array.clear()
+                    temp_string = self.my_frame_string
+                    self.start_time = frame.start_time
+                    self.lt_end_time = frame.start_time
+                    self.my_frame_string = None
+                    self.my_frame_string = str()
+                    return AnalyzerFrame("LT", self.lt_start_time,frame.end_time, {
+                        'input_type': temp_string
                     })
 
         else:
@@ -86,7 +104,6 @@ class Hla(HighLevelAnalyzer):
         # Return the data frame itself
 
     def broadcast_or_addressed_RT_decode(self):
-        print('RT')
         stx_symbol = int.from_bytes(
             self.raw_data_array[0], byteorder='big')
         if (stx_symbol & 0x20) == 0x20:
@@ -98,30 +115,23 @@ class Hla(HighLevelAnalyzer):
         self.my_frame_string+= ' addressed RT'
         stx_symbol = int.from_bytes(
             self.raw_data_array[0], byteorder='big')
-        data = int()
-        for x in range(1,len(self.raw_data_array)):
-            data[x] = int.from_bytes(self.raw_data_array[x],byteorder='big')
+        data = [0]*(len(self.raw_data_array)-2)
+        
+        for x in range(1,len(self.raw_data_array)-2):
+            data[x-1] = int.from_bytes(self.raw_data_array[x],byteorder='big')
         if stx_symbol & 0x01:
             self.my_frame_string += ' response'
         else:
             self.my_frame_string += ' command'
         self.my_frame_string += ' index:'
         self.my_frame_string += str((stx_symbol & 0x1e) >> 1)
-    def AT_decode(self):
-        self.my_frame_string+= ' AT'
-        start_transaction_symbol = int.from_bytes(
-            self.raw_data_array[0], byteorder='big')
-        data = [0]*(len(self.raw_data_array)-2)
-        for x in range(1,len(self.raw_data_array)-2):
-            data[x-1] = int.from_bytes(self.raw_data_array[x],byteorder='big')
-        if start_transaction_symbol & 1:
-            self.my_frame_string += ' command '
+        if (data[1]& 0x80) == 0x80:
+            self.my_frame_string += ' write'
         else:
-            self.my_frame_string += ' response '
-        if start_transaction_symbol & 4:
-            self.my_frame_string += ' final recipient '
+            self.my_frame_string += ' read'
         self.my_frame_string += ' reg:'
         self.my_frame_string += hex(data[0])
+
         if data[0] == 0:
             self.my_frame_string += ' vendor id reg'
         elif data[0] == 1:
@@ -140,9 +150,58 @@ class Hla(HighLevelAnalyzer):
         self.my_frame_string += ' len:'
         self.my_frame_string += str(data[1])
         self.my_frame_string += ' data:'
+
         for x in range(2,len(self.raw_data_array)-3):
             self.my_frame_string += hex(data[x])
             self.my_frame_string += ','            
+        
+
+    def AT_decode(self):
+        self.my_frame_string+= ' AT'
+        start_transaction_symbol = int.from_bytes(
+            self.raw_data_array[0], byteorder='big')
+        data = [0]*(len(self.raw_data_array)-2)
+        for x in range(1,len(self.raw_data_array)-2):
+            data[x-1] = int.from_bytes(self.raw_data_array[x],byteorder='big')
+
+        if start_transaction_symbol & 1:
+            self.my_frame_string += ' command '
+        else:
+            self.my_frame_string += ' response '
+
+        if start_transaction_symbol & 4:
+            self.my_frame_string += ' final recipient '
+
+        if (data[1]& 0x80) == 0x80:
+            self.my_frame_string += ' write'
+        else:
+            self.my_frame_string += ' read'
+        self.my_frame_string += ' reg:'
+        self.my_frame_string += hex(data[0])
+
+        if data[0] == 0:
+            self.my_frame_string += ' vendor id reg'
+        elif data[0] == 1:
+            self.my_frame_string += ' product id reg'
+        elif data[0] == 8:
+            self.my_frame_string += ' op code reg'
+        elif data[0] == 9:
+            self.my_frame_string += ' meta data reg'
+        elif data[0] == 12:
+            self.my_frame_string += ' link config reg'
+        elif data[0] == 13:
+            self.my_frame_string += ' Tx FFE reg'
+        else:
+            self.my_frame_string += ' undefined reg'
+ 
+        self.my_frame_string += ' len:'
+        self.my_frame_string += str(data[1])
+        self.my_frame_string += ' data:'
+
+        for x in range(2,len(self.raw_data_array)-3):
+            self.my_frame_string += hex(data[x])
+            self.my_frame_string += ','            
+
     def RT_decode(self):
         self.my_frame_string+= ' broadcast RT'
         start_transaction_symbol = int.from_bytes(
@@ -155,25 +214,31 @@ class Hla(HighLevelAnalyzer):
             self.my_frame_string += ' RT command'
         else:
             self.my_frame_string += ' RT response'
+
         if start_transaction_symbol & 0x20:
             self.my_frame_string += ' RT broadcast'
+
         if link_parameters_LSB & 0x01:
             self.my_frame_string += ' USB4'
+
         if link_parameters_LSB & 0x04:
             self.my_frame_string += ' RS FEC enabled'
+
         if link_parameters_LSB & 0x08:
             self.my_frame_string += ' SSC on'
         else:
             self.my_frame_string += ' SSC off'
+
         if link_parameters_HSB & 0x01:
             self.my_frame_string += ' lane 0 on'
+
         if link_parameters_HSB & 0x02:
             self.my_frame_string += ' lane 1 on'
+
         if (link_parameters_HSB & 0xf0) == 0x10:
             self.my_frame_string += ' gen 2 speed'
         elif (link_parameters_HSB & 0xf0) == 0x20:
             self.my_frame_string += ' gen 3 speed'
-        print(self.my_frame_string)
 
     def LT_decode(self):
         self.my_frame_string+= ' LT'
